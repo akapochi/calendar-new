8fc2033378164769b8f3aedc99d883a6fd5a61fa
Soma <redstar.ihsohaka@gmail.com>
2021-02-03 11:06:36 +0900

    databaseの名前を変更

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -2,7 +2,7 @@
 require('pg').defaults.ssl = true;
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/calendar'
+  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/eventmanagement'
 );
 
 module.exports = {
