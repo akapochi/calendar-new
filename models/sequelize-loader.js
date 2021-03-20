c934daf6769aca160a335eb904fde92c7919d5d3
Soma <redstar.ihsohaka@gmail.com>
2021-03-20 09:57:18 +0900

    ReferenceErrorを解決

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,4 +1,5 @@
 'use strict';
+const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/eventmanagement',
   {
