d307e14310d82d4a8a2098995d14dbf28ae59103
Soma <redstar.ihsohaka@gmail.com>
2021-03-20 09:55:24 +0900

    constが2つになっていたので1つに変更

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,17 +1,17 @@
 'use strict';
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/schedule_arranger',
+  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/eventmanagement',
   {
     dialectOptions: {
       ssl: true,
     },
   }
 );
-require('pg').defaults.ssl = true;
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/eventmanagement'
-);
+// require('pg').defaults.ssl = true;
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize(
+//   process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/eventmanagement'
+// );
 
 module.exports = {
   database: sequelize,
