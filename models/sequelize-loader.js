08faf50469010bbad32e44747d7d035612a64ece
Soma <redstar.ihsohaka@gmail.com>
2021-03-20 09:53:51 +0900

    フォーラムで見た解決案を実装

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,4 +1,12 @@
 'use strict';
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/schedule_arranger',
+  {
+    dialectOptions: {
+      ssl: true,
+    },
+  }
+);
 require('pg').defaults.ssl = true;
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
