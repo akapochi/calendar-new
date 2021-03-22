a7cba04147da38295d1d68f70222f6aadd7be741
Soma <redstar.ihsohaka@gmail.com>
2021-03-22 23:46:56 +0900

    index.jsを元に戻した

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,18 +1,25 @@
 'use strict';
-// const Sequelize = require('sequelize');
-// const sequelize = new Sequelize(
-//   process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/eventmanagement',
-//   {
-//     dialectOptions: {
-//       ssl: true,
-//     },
-//   }
-// );
-require('pg').defaults.ssl = true;
+
+if (process.env.DATABASE_URL) {
+  const pg = require('pg');
+  pg.defaults.ssl = true;
+}
+
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/eventmanagement'
+  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/eventmanagement',
+  {
+    dialectOptions: {
+      ssl: true,
+    },
+  }
 );
+
+// require('pg').defaults.ssl = true;
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize(
+//   process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/eventmanagement'
+// );
 
 module.exports = {
   database: sequelize,
